184dfba35222431510aa19d63738a62b4d4c7270
Olivier Lenoir <olenoir@wynd.eu>
2018-12-10 12:52:54 +0100

    Day 1

---
+++ Day01/day01.lua
@@ -0,0 +1,29 @@
+function part_one()
+  frequency = 0
+  for line in io.lines("Day01/input.txt") do
+    frequency = frequency + line
+  end
+  return frequency
+end
+
+function part_two()
+  frequency = 0
+  index = 1
+  foundFrequencies = {}
+  boucle = 1
+  while boucle == 1 do
+    for line in io.lines("Day01/input.txt") do
+      frequency = frequency + line
+      foundFrequencies[index] = frequency
+      for i = 1, index - 1 do
+        if frequency == foundFrequencies[i] then
+          return frequency
+        end
+      end
+    index = index + 1
+    end
+  end
+end
+
+print ("The resulting frequency is " .. part_one())
+print ("The first frequency reached twice is " .. part_two())
